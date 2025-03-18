1a7367dfd2421e41baab2675f793821231dc1169
Stacey jepchumba <satceyjepchumbarr@gamil.com>
2025-03-18 12:31:04 +0300

    added new changes

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 7.75
+let num2 = 8
+let multiply = num1*num2
+let random =Math.floor(Math.random()*100)+1;
+let num3 =20
+let num4=8
+let mod =num3%num4
+let max =Math.max(num1, num2, num3, num4);
